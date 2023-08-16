7544b63728900a03db475be55c2f0a0292f611d0
Davidvl <josuedavidvl18@gmail.com>
2023-08-15 21:17:52 -0600

    Cambios en la UI

--- app.js
+++ app.js
@@ -43,6 +43,30 @@ class UI {
         document.getElementById('cantidad').value = '';
     }
 
+     static actualizarBalanceEnTabla() {
+        const transacciones = Almacenamiento.obtenerTransacciones();
+        let totalIngresos = 0;
+        let totalGastos = 0;
+
+        transacciones.forEach(transaccion => {
+            if (transaccion.tipo === 'ingreso') {
+                totalIngresos += transaccion.cantidad;
+            } else {
+                totalGastos -= transaccion.cantidad;  // Usamos -= para restar el gasto
+            }
+        });
+
+        let balance = totalIngresos + totalGastos;  // En totalGastos ya está el valor negativo
+
+        const tablaTransacciones = document.getElementById('tablaTransacciones');
+        const filaBalance = document.createElement('tr');
+        filaBalance.innerHTML = `
+            <td colspan="3"><b>Balance</td>
+            <td>Q${balance.toFixed(2)}</td>
+        `;
+        tablaTransacciones.appendChild(filaBalance);
+    }
+
     static llenarTabla(desde, hasta) {
         const tablaTransacciones = document.getElementById('tablaTransacciones');
         const transacciones = Almacenamiento.obtenerTransacciones();
@@ -63,6 +87,8 @@ class UI {
             `;
             tablaTransacciones.appendChild(fila);
         });
+
+        UI.actualizarBalanceEnTabla();  // Llamar a la función después de llenar la tabla
     }
 }
 
@@ -92,27 +118,53 @@ function actualizarGrafico() {
     if(window.miGrafico) {
         window.miGrafico.destroy(); // Destruimos el gráfico anterior para evitar superposiciones
     }
+
     const transacciones = Almacenamiento.obtenerTransacciones();
     const ingresos = _.filter(transacciones, { 'tipo': 'ingreso' });
     const gastos = _.filter(transacciones, { 'tipo': 'gasto' });
 
+    const totalIngresos = _.sumBy(ingresos, 'cantidad');
+    const totalGastos = _.sumBy(gastos, 'cantidad');
+
     const etiquetas = ['Ingresos', 'Gastos'];
-    const datos = [_.sumBy(ingresos, 'cantidad'), _.sumBy(gastos, 'cantidad')];
+    const datos = [totalIngresos, totalGastos];
 
     const contexto = document.getElementById('graficoFinanzas').getContext('2d');
 
     window.miGrafico = new Chart(contexto, {
-        type: 'doughnut',
+        type: 'bar',
         data: {
             labels: etiquetas,
             datasets: [{
                 label: 'Finanzas Personales',
                 data: datos,
-                backgroundColor: ['rgba(0, 255, 0, 0.6)', 'rgba(255, 0, 0, 0.6)'],
+                backgroundColor: ['#4CAF50', '#FF5733'],
+                hoverBackgroundColor: ['#45A049', '#FF4136']
             }]
+        },
+        options: {
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        let label = data.labels[tooltipItem.index] || '';
+                        let value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+                        label += `: $${value.toFixed(2)}`;
+                        return label;
+                    }
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
     });
 }
+
+
 
 document.addEventListener('DOMContentLoaded', UI.mostrarTransacciones);
 document.addEventListener('DOMContentLoaded', UI.llenarTabla);
